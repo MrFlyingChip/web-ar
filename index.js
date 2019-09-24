6c74938b1a96973747a13fafca59410fe2ba7bb0
Kirill Minaev <kv_l@ukr.net>
2019-09-24 17:03:24 +0300

    motion test

---
+++ index.js
@@ -0,0 +1,16 @@
+let output = document.querySelector('.output');
+
+function handleOrientation(event) {
+    let absolute = event.absolute;
+    let alpha    = event.alpha;
+    let beta     = event.beta;
+    let gamma    = event.gamma;
+
+    output.innerHTML  = "absolute : " + absolute + "\n";
+    output.innerHTML += "alpha: " + alpha + "\n";
+    output.innerHTML += "beta: " + beta + "\n";
+    output.innerHTML += "gamma: " + gamma + "\n";
+}
+
+
+window.addEventListener('deviceorientation', handleOrientation);
